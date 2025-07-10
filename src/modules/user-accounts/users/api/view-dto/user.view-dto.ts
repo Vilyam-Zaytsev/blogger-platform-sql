8021dee313894695ebc50d0766b1ce61b94faa2b
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-07-10 19:32:19 +0300

    feat(users) реализовал метод mapToView для UserViewDto

--- src/modules/user-accounts/users/api/view-dto/user.view-dto.ts
+++ src/modules/user-accounts/users/api/view-dto/user.view-dto.ts
@@ -1,4 +1,5 @@
 import { OmitType } from '@nestjs/swagger';
+import { UserDbType } from '../../types/user-db.type';
 
 export class UserViewDto {
   id: string;
@@ -6,16 +7,16 @@ export class UserViewDto {
   login: string;
   createdAt: string;
 
-  // static mapToView(user: UserDocument): UserViewDto {
-  //   const dto = new this();
-  //
-  //   dto.id = user._id.toString();
-  //   dto.login = user.login;
-  //   dto.email = user.email;
-  //   dto.createdAt = user.createdAt.toISOString();
-  //
-  //   return dto;
-  // }
+  static mapToView(user: UserDbType): UserViewDto {
+    const dto = new this();
+
+    dto.id = user.id.toString();
+    dto.login = user.login;
+    dto.email = user.email;
+    dto.createdAt = user.createdAt;
+
+    return dto;
+  }
 }
 
 export class MeViewDto extends OmitType(UserViewDto, [
@@ -24,7 +25,7 @@ export class MeViewDto extends OmitType(UserViewDto, [
 ] as const) {
   userId: string;
 
-  // static mapToView(user: UserDocument): MeViewDto {
+  // static mapToView(user: UserDbType): MeViewDto {
   //   const dto = new this();
   //
   //   dto.email = user.email;
