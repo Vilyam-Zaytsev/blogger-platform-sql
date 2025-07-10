beb3563873454c400fdd715513edc3c4d10f8ef7
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-07-10 21:34:58 +0300

    feat(users) реализовал метод getAll для UsersController

--- src/modules/user-accounts/users/api/users.controller.ts
+++ src/modules/user-accounts/users/api/users.controller.ts
@@ -1,18 +1,29 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { UserInputDto } from './input-dto/user.input-dto';
 import { UserViewDto } from './view-dto/user.view-dto';
-import { CommandBus } from '@nestjs/cqrs';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreateUserCommand } from '../application/usecases/create-user-by-admin.usecase';
 import { UsersRepository } from '../infrastructure/users.repository';
 import { UsersQueryRepository } from '../infrastructure/query/users.query-repository';
+import { GetUsersQueryParams } from './input-dto/get-users-query-params.input-dto';
+import { PaginatedViewDto } from '../../../../core/dto/paginated.view-dto';
+import { GetUsersQuery } from '../application/queries/get-users.query-handler';
 
 @Controller('users')
 export class UsersController {
   constructor(
+    private readonly queryBus: QueryBus,
     private readonly commandBus: CommandBus,
     private readonly usersRepository: UsersRepository,
     private readonly usersQueryRepository: UsersQueryRepository,
   ) {}
+
+  @Get()
+  async getAll(
+    @Query() query: GetUsersQueryParams,
+  ): Promise<PaginatedViewDto<UserViewDto>> {
+    return this.queryBus.execute(new GetUsersQuery(query));
+  }
 
   @Post()
   async createUser(@Body() body: UserInputDto): Promise<UserViewDto> {
