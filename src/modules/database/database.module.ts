aacc24a6fa995fbfe90fc26f3897ed58505e0314
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-07-10 18:23:04 +0300

    chore(DatabaseModule) зарегистрировал провайдоры в DatabaseModule

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -2,15 +2,11 @@ import { Global, Module } from '@nestjs/common';
 import { DatabaseConfig } from './config/database.config';
 import { PgPoolProvider } from './providers/pg-pool.provider';
 import { PG_POOL } from './constants/database.constants';
+import { MigrationRunnerService } from './migration-runner.service';
 
 @Global()
 @Module({
-  providers: [
-    //🔸 Common:
-    //config
-    DatabaseConfig,
-    PgPoolProvider,
-  ],
+  providers: [DatabaseConfig, PgPoolProvider, MigrationRunnerService],
   exports: [PG_POOL],
 })
 export class DatabaseModule {}
