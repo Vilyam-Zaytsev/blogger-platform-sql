c3884be691a1fd79f0c0eb6825e8043239331677
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-07-10 19:54:19 +0300

    chore(exceptions) зарегистрировал в провайдеры AppModule exceptions фильтры

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,28 @@ import { CoreModule } from './core/core.module';
 import { TestingModule } from './modules/testing/testing.module';
 import { DatabaseModule } from './modules/database/database.module';
 import { UserAccountsModule } from './modules/user-accounts/user-accounts.module';
+import { APP_FILTER } from '@nestjs/core';
+import { AllHttpExceptionsFilter } from './core/exceptions/filters/all-exceptions.filter';
+import { DomainHttpExceptionsFilter } from './core/exceptions/filters/domain-exceptions.filter';
+import { ValidationExceptionFilter } from './core/exceptions/filters/validation-exception.filter';
 
 @Module({
   imports: [configModule, CoreModule, DatabaseModule, UserAccountsModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllHttpExceptionsFilter,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: DomainHttpExceptionsFilter,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ValidationExceptionFilter,
+    },
+  ],
 })
 export class AppModule {
   static forRoot(coreConfig: CoreConfig): DynamicModule {
