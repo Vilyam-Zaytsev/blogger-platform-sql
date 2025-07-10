c2c68cff094e9c4453953b62128b280c52ec7ba4
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-07-10 18:20:26 +0300

    chore(users) добавил провайдеры в модуль

--- src/modules/user-accounts/user-accounts.module.ts
+++ src/modules/user-accounts/user-accounts.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users/api/users.controller';
+import { UsersRepository } from './users/infrastructure/users.repository';
+import { CreateUserByAdminUseCase } from './users/application/usecases/create-user-by-admin.usecase';
+import { UserValidationService } from './users/application/services/user-validation.service';
+import { CryptoService } from './users/application/services/crypto.service';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [UsersController],
+  providers: [
+    //🔸 User:
+    //use-cases
+    CreateUserByAdminUseCase,
+    //services
+    CryptoService,
+    UserValidationService,
+    //repo
+    UsersRepository,
+  ],
   exports: [],
 })
 export class UserAccountsModule {}
