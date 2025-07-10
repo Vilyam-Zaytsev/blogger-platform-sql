9cb3ae280cadff442c1f271c1cc776a2cce8f568
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-07-10 19:28:41 +0300

    feat(users) реализовал метод createUser для UsersController

--- src/modules/user-accounts/users/api/users.controller.ts
+++ src/modules/user-accounts/users/api/users.controller.ts
@@ -1,13 +1,22 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserInputDto } from './input-dto/user.input-dto';
+import { UserViewDto } from './view-dto/user.view-dto';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateUserCommand } from '../application/usecases/create-user-by-admin.usecase';
+import { UsersRepository } from '../infrastructure/users.repository';
+import { UsersQueryRepository } from '../infrastructure/query/users.query-repository';
 
 @Controller('users')
 export class UsersController {
-  constructor() {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly usersRepository: UsersRepository,
+    private readonly usersQueryRepository: UsersQueryRepository,
+  ) {}
 
   @Post()
   async createUser(@Body() body: UserInputDto): Promise<UserViewDto> {
-    const userId: string = await this.commandBus.execute(
+    const userId: number = await this.commandBus.execute(
       new CreateUserCommand(body),
     );
 
