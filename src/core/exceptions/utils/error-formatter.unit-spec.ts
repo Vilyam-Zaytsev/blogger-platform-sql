fb8e0dbd166584085b8aa9b7a9f9c65d81bfd934
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-07-10 20:35:25 +0300

    test(errorFormatter) добавил unit тесты для errorFormatter

---
+++ src/core/exceptions/utils/error-formatter.unit-spec.ts
@@ -0,0 +1,76 @@
+import { ValidationError } from '@nestjs/common';
+import { errorFormatter } from './error-formatter';
+import { Extension } from '../damain-exceptions';
+
+describe('errorFormatter', () => {
+  it('should format flat errors with constrains', () => {
+    const input: ValidationError[] = [
+      {
+        property: 'email',
+        value: 'invalid-email',
+        constraints: {
+          isEmail: 'Email must be valid',
+        },
+        children: [],
+      },
+    ];
+
+    const result: Extension[] = errorFormatter(input);
+
+    expect(result).toEqual([
+      {
+        field: 'email',
+        message: 'Email must be valid; Received value: invalid-email',
+      },
+    ]);
+  });
+
+  it('should handle nested validation errors', () => {
+    const input: ValidationError[] = [
+      {
+        property: 'user',
+        value: undefined,
+        constraints: undefined,
+        children: [
+          {
+            property: 'name',
+            value: '',
+            constraints: {
+              isNotEmpty: 'Name should not be empty',
+            },
+            children: [],
+          },
+        ],
+      },
+    ];
+
+    const result = errorFormatter(input);
+
+    expect(result).toEqual([
+      {
+        field: 'name',
+        message: 'Name should not be empty; Received value: ',
+      },
+    ]);
+  });
+
+  it('should return an empty array if no errors', () => {
+    const result: Extension[] = errorFormatter([]);
+    expect(result).toEqual([]);
+  });
+
+  it('should ignore errors without constraints or children', () => {
+    const input: ValidationError[] = [
+      {
+        property: 'age',
+        value: 42,
+        constraints: undefined,
+        children: [],
+      },
+    ];
+
+    const result: Extension[] = errorFormatter(input);
+
+    expect(result).toEqual([]);
+  });
+});
