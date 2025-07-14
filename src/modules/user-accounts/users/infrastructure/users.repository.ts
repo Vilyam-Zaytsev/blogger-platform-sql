80b1b704754ed93e0fd632f375bbc166daafa1bf
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-07-14 21:26:06 +0300

    feat(UsersRepository): добавил методы: getByIdOrNotFoundFail, softDelete

--- src/modules/user-accounts/users/infrastructure/users.repository.ts
+++ src/modules/user-accounts/users/infrastructure/users.repository.ts
@@ -1,9 +1,14 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { PG_POOL } from '../../../database/constants/database.constants';
-import { Pool, QueryResult } from 'pg';
+import { Pool, PoolClient, QueryResult } from 'pg';
 import { CreateUserDto } from '../dto/create-user.dto';
-import { ConfirmationStatus } from '../types/email-confirmation-db.type';
+import {
+  ConfirmationStatus,
+  EmailConfirmationDbType,
+} from '../types/email-confirmation-db.type';
 import { UserDbType } from '../types/user-db.type';
+import { DomainException } from 'src/core/exceptions/damain-exceptions';
+import { DomainExceptionCode } from '../../../../core/exceptions/domain-exception-codes';
 
 @Injectable()
 export class UsersRepository {
@@ -12,8 +17,7 @@ export class UsersRepository {
   async insertUser(dto: CreateUserDto): Promise<number> {
     const query = `
         INSERT INTO "Users" ("login", "email", "passwordHash")
-        VALUES ($1, $2, $3)
-        RETURNING *;
+        VALUES ($1, $2, $3) RETURNING *;
     `;
 
     const values: string[] = [dto.login, dto.email, dto.passwordHash];
@@ -30,12 +34,10 @@ export class UsersRepository {
     userId: number,
   ): Promise<void> {
     const query = `
-        INSERT INTO "EmailConfirmation" (
-            "userId",
-            "confirmationCode", 
-            "expirationDate", 
-            "confirmationStatus"
-        )
+        INSERT INTO "EmailConfirmation" ("userId",
+                                         "confirmationCode",
+                                         "expirationDate",
+                                         "confirmationStatus")
         VALUES ($1, NULL, NULL, $2)
     `;
 
@@ -43,21 +45,26 @@ export class UsersRepository {
 
     await this.pool.query(query, values);
   }
-  // async getByIdOrNotFoundFail(id: string): Promise<UserDocument> {
-  //   const user: UserDocument | null = await this.UserModel.findOne({
-  //     _id: id,
-  //     deletedAt: null,
-  //   });
-  //
-  //   if (!user) {
-  //     throw new DomainException({
-  //       code: DomainExceptionCode.NotFound,
-  //       message: `The user with ID (${id}) does not exist`,
-  //     });
-  //   }
-  //
-  //   return user;xxxxxxxxxxxxxxxxxxxxxxxx
-  // }
+
+  async getByIdOrNotFoundFail(id: number): Promise<UserDbType> {
+    const user: QueryResult<UserDbType> = await this.pool.query(
+      `SELECT *
+       FROM "Users"
+       WHERE id = $1
+         AND "deletedAt" IS NULL`,
+      [id],
+    );
+
+    if (!user.rows[0]) {
+      throw new DomainException({
+        code: DomainExceptionCode.NotFound,
+        message: `The user with ID (${id}) does not exist`,
+      });
+    }
+
+    return user.rows[0];
+  }
+
   //
   // async getByConfirmationCode(
   //   confirmationCode: string,
@@ -105,6 +112,85 @@ export class UsersRepository {
     }
 
     return result.rows[0] as UserDbType;
+  }
+
+  //TODO: Нормально ли в этой ситуации то, что репозиторий отвечает за логику приложения?
+  //TODO: Нормально ли в этом случае использовать транзакцию или лучше разделить на два метода?
+
+  async softDelete(id: number): Promise<UserDbType | null> {
+    const client: PoolClient = await this.pool.connect();
+
+    try {
+      await client.query('BEGIN');
+
+      const userResult: QueryResult<UserDbType> = await client.query(
+        `UPDATE "Users"
+         SET "deletedAt" = NOW()
+         WHERE id = $1
+           AND "deletedAt" IS NULL RETURNING *;`,
+        [id],
+      );
+
+      const user: UserDbType = userResult.rows[0];
+
+      if (!user) {
+        await client.query('ROLLBACK');
+        return null;
+      }
+
+      if (userResult.rowCount === 0) {
+        await client.query('ROLLBACK');
+        throw new DomainException({
+          code: DomainExceptionCode.NotFound,
+          message: 'The user with ID (${id}) does not exist',
+        });
+      }
+
+      // await client.query(
+      //   `UPDATE "EmailConfirmation"
+      //    SET "deletedAt" = NOW()
+      //    WHERE "userId" = $1
+      //      AND "deletedAt" IS NULL;`,
+      //   [id],
+      // );
+
+      const emailConfirmationResult: QueryResult<EmailConfirmationDbType> =
+        await client.query(
+          `UPDATE "EmailConfirmation"
+           SET "deletedAt" = NOW()
+           WHERE "userId" = $1
+             AND "deletedAt" IS NULL;`,
+          [id],
+        );
+
+      if (emailConfirmationResult.rowCount === 0) {
+        await client.query('ROLLBACK');
+        throw new DomainException({
+          code: DomainExceptionCode.InternalServerError,
+          message:
+            'The user was not deleted because the associated EmailConfirmation was not found',
+        });
+      }
+
+      await client.query('COMMIT');
+      return user;
+    } catch (error) {
+      console.error(
+        'Ошибка при выполнении SQL-запроса в UsersRepository.softDelete():',
+        error,
+      );
+
+      if (error instanceof DomainException) {
+        throw error;
+      }
+
+      throw new DomainException({
+        code: DomainExceptionCode.InternalServerError,
+        message: "Couldn't delete user",
+      });
+    } finally {
+      client.release();
+    }
   }
 
   // async getByIds(ids: string[]): Promise<UserDocument[]> {
