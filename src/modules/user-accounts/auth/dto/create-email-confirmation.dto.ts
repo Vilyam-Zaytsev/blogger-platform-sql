0402e7d938d3645e8be1dd4b6111a5b1bc0e5ada
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-07-16 21:35:52 +0300

    refactor(auth): изменил типизацию полей в CreateEmailConfirmationDto

--- src/modules/user-accounts/auth/dto/create-email-confirmation.dto.ts
+++ src/modules/user-accounts/auth/dto/create-email-confirmation.dto.ts
@@ -2,7 +2,7 @@ import { ConfirmationStatus } from '../../users/types/email-confirmation-db.type
 
 export class CreateEmailConfirmationDto {
   userId: number;
-  confirmationCode: string;
-  expirationDate: Date;
+  confirmationCode: string | null;
+  expirationDate: Date | null;
   confirmationStatus: ConfirmationStatus;
 }
