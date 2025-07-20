525568cdcb7a03901944530ed5adc59b705d98d0
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-07-20 10:48:33 +0300

    feat(db): реализовал AppTestManager для управления тестовып окружением

---
+++ test/managers/app.test-manager.ts
@@ -0,0 +1,92 @@
+import { DynamicModule, INestApplication } from '@nestjs/common';
+import { Test, TestingModuleBuilder } from '@nestjs/testing';
+import { appSetup } from '../../src/setup/app.setup';
+import { Server } from 'http';
+import { CoreConfig } from '../../src/core/core.config';
+import { initAppModule } from '../../src/init-app-module';
+import { ThrottlerStorage } from '@nestjs/throttler';
+import { Pool } from 'pg';
+import { PG_POOL } from '../../src/modules/database/constants/database.constants';
+import { AdminCredentials, MemoryThrottlerStorageLike } from '../types';
+
+export class AppTestManager {
+  app: INestApplication;
+  pool: Pool;
+  coreConfig: CoreConfig;
+
+  async init(
+    addSettingsToModuleBuilder?: (moduleBuilder: TestingModuleBuilder) => void,
+  ) {
+    const DynamicAppModule: DynamicModule = await initAppModule();
+
+    const testingModuleBuilder: TestingModuleBuilder = Test.createTestingModule(
+      {
+        imports: [DynamicAppModule],
+      },
+    );
+
+    if (addSettingsToModuleBuilder) {
+      addSettingsToModuleBuilder(testingModuleBuilder);
+    }
+
+    const testingAppModule = await testingModuleBuilder.compile();
+
+    this.app = testingAppModule.createNestApplication();
+
+    this.coreConfig = this.app.get<CoreConfig>(CoreConfig);
+    this.pool = this.app.get<Pool>(PG_POOL);
+
+    appSetup(this.app, this.coreConfig.isSwaggerEnabled);
+
+    await this.app.init();
+  }
+
+  async cleanupDb() {
+    const tables = await this.pool.query(
+      "SELECT table_name FROM information_schema.tables WHERE table_schema='public'",
+    );
+
+    await Promise.all(
+      tables.rows.map(async (table) => {
+        await this.pool.query(`DELETE FROM "${table.table_name}"`);
+      }),
+    );
+  }
+
+  clearThrottlerStorage() {
+    const throttlerStorage: ThrottlerStorage =
+      this.app.get<ThrottlerStorage>(ThrottlerStorage);
+
+    const memoryStorage = throttlerStorage as MemoryThrottlerStorageLike;
+
+    if (memoryStorage.storage instanceof Map) {
+      memoryStorage.storage.clear();
+    }
+  }
+
+  async close() {
+    await this.pool.end();
+    await this.app.close();
+  }
+
+  getServer() {
+    return this.app.getHttpServer() as Server;
+  }
+
+  getAdminCredentials(): AdminCredentials {
+    const login: string | undefined = this.coreConfig.adminLogin;
+    const password: string | undefined = this.coreConfig.adminPassword;
+
+    if (!login || !password) {
+      throw new Error(
+        'Admin credentials are not configured properly: ' +
+          'ADMIN_LOGIN and/or ADMIN_PASSWORD environment variables are missing or empty.',
+      );
+    }
+
+    return {
+      login,
+      password,
+    };
+  }
+}
