530f3cae0f86a1f8c01ec23fff0473b87ffa9ed4
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-07-27 22:16:07 +0300

    feat(sessions): реализовал SessionsQueryRepository си добал в него метод для выборки всех сессий по фильтру userId

---
+++ src/modules/user-accounts/sessions/infrastructure/query/sessions.query-repository.ts
@@ -0,0 +1,24 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { SessionViewDto } from '../../api/view-dto/session.view-dto';
+import { PG_POOL } from '../../../../database/constants/database.constants';
+import { Pool } from 'pg';
+import { SessionDbType } from '../../../auth/types/session-db.type';
+
+@Injectable()
+export class SessionsQueryRepository {
+  constructor(@Inject(PG_POOL) private readonly pool: Pool) {}
+
+  async getAllByUserId(userId: number): Promise<SessionViewDto[]> {
+    const { rows } = await this.pool.query(
+      `SELECT *
+       FROM "Sessions"
+       WHERE "userId" = $1`,
+      [userId],
+    );
+
+    return rows.map(
+      (session: SessionDbType): SessionViewDto =>
+        SessionViewDto.mapToView(session),
+    );
+  }
+}
