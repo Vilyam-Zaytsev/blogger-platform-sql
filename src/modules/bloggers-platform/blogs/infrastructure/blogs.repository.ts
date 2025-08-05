c383a044b18ec25bb3b04dbfbe16854cf6679511
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-08-05 12:41:17 +0300

    feat(BlogsRepository): добавил методы getByIdOrNotFoundFail и updateBlog

--- src/modules/bloggers-platform/blogs/infrastructure/blogs.repository.ts
+++ src/modules/bloggers-platform/blogs/infrastructure/blogs.repository.ts
@@ -1,8 +1,11 @@
 import { Pool, QueryResult } from 'pg';
-import { CreateBlogDto } from '../dto/blog.dto';
+import { CreateBlogDto } from '../dto/create-blog.dto';
 import { Inject } from '@nestjs/common';
 import { PG_POOL } from '../../../database/constants/database.constants';
 import { BlogDbType } from '../types/blog-db.type';
+import { DomainException } from '../../../../core/exceptions/domain-exceptions';
+import { DomainExceptionCode } from '../../../../core/exceptions/domain-exception-codes';
+import { UpdateBlogDto } from '../dto/update-blog.dto';
 
 export class BlogsRepository {
   constructor(@Inject(PG_POOL) private readonly pool: Pool) {}
@@ -19,25 +22,39 @@ export class BlogsRepository {
     return rows[0].id;
   }
 
-  // async getByIdOrNotFoundFail(id: string): Promise<BlogDocument> {
-  //   const blog: BlogDocument | null = await this.BlogModel.findOne({
-  //     _id: id,
-  //     deletedAt: null,
-  //   });
-  //
-  //   if (!blog) {
-  //     throw new DomainException({
-  //       code: DomainExceptionCode.NotFound,
-  //       message: `The blog with ID (${id}) does not exist`,
-  //     });
-  //   }
-  //
-  //   return blog;
-  // }
-  //
-  // async save(blog: BlogDocument): Promise<string> {
-  //   const resultSave: BlogDocument = await blog.save();
-  //
-  //   return resultSave._id.toString();
-  // }
+  async getByIdOrNotFoundFail(id: number): Promise<BlogDbType> {
+    const { rows }: QueryResult<BlogDbType> = await this.pool.query(
+      `
+        SELECT *
+        FROM "Blogs"
+        WHERE "id" = $1
+          AND "deletedAt" IS NULL
+      `,
+      [id],
+    );
+
+    if (rows.length === 0) {
+      throw new DomainException({
+        code: DomainExceptionCode.NotFound,
+        message: `The blog with ID (${id}) does not exist`,
+      });
+    }
+
+    return rows[0];
+  }
+
+  async updateBlog(dto: UpdateBlogDto): Promise<void> {
+    const { id, name, description, websiteUrl }: UpdateBlogDto = dto;
+
+    await this.pool.query(
+      `
+    UPDATE "Blogs"
+    SET "name" = $1,
+        "description" = $2,
+        "websiteUrl" = $3
+    WHERE "id" = $4
+    `,
+      [name, description, websiteUrl, id],
+    );
+  }
 }
