81485500343bc5c613c876de2a2e6fa9b6f70679
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-08-06 10:15:08 +0300

    test(manager): реализовал метод createBlog() для BlogsTestManager

--- test/managers/blogs.test-manager.ts
+++ test/managers/blogs.test-manager.ts
@@ -5,6 +5,8 @@ import { HttpStatus } from '@nestjs/common';
 import request, { Response } from 'supertest';
 import { GetBlogsQueryParams } from '../../src/modules/bloggers-platform/blogs/api/input-dto/get-blogs-query-params.input-dto';
 import { PaginatedViewDto } from '../../src/core/dto/paginated.view-dto';
+import { BlogInputDto } from '../../src/modules/bloggers-platform/blogs/api/input-dto/blog-input.dto';
+import { TestDtoFactory } from '../helpers/test.dto-factory';
 
 export class BlogsTestManager {
   constructor(
@@ -12,33 +14,33 @@ export class BlogsTestManager {
     private readonly adminCredentialsInBase64: string,
   ) {}
 
-  // async createBlog(quantity: number): Promise<BlogViewDto[]> {
-  //   const newBlogs: BlogViewDto[] = [];
-  //   const dtos: BlogInputDto[] = TestDtoFactory.generateBlogInputDto(quantity);
-  //
-  //   for (let i = 0; i < quantity; i++) {
-  //     const blog: BlogInputDto = dtos[i];
-  //
-  //     const response: Response = await request(this.server)
-  //       .post(`/${GLOBAL_PREFIX}/blogs`)
-  //       .send(blog)
-  //       .set('Authorization', this.adminCredentialsInBase64)
-  //       .expect(HttpStatus.CREATED);
-  //
-  //     const newBlog: BlogViewDto = response.body as BlogViewDto;
-  //
-  //     expect(typeof newBlog.id).toBe('string');
-  //     expect(new Date(newBlog.createdAt).toString()).not.toBe('Invalid Date');
-  //     expect(newBlog.name).toBe(blog.name);
-  //     expect(newBlog.description).toBe(blog.description);
-  //     expect(newBlog.websiteUrl).toBe(blog.websiteUrl);
-  //     expect(typeof newBlog.isMembership).toBe('boolean');
-  //
-  //     newBlogs.push(newBlog);
-  //   }
-  //
-  //   return newBlogs;
-  // }
+  async createBlog(quantity: number): Promise<BlogViewDto[]> {
+    const newBlogs: BlogViewDto[] = [];
+    const dtos: BlogInputDto[] = TestDtoFactory.generateBlogInputDto(quantity);
+
+    for (let i = 0; i < quantity; i++) {
+      const blog: BlogInputDto = dtos[i];
+
+      const response: Response = await request(this.server)
+        .post(`/${GLOBAL_PREFIX}/sa/blogs`)
+        .send(blog)
+        .set('Authorization', this.adminCredentialsInBase64)
+        .expect(HttpStatus.CREATED);
+
+      const newBlog: BlogViewDto = response.body as BlogViewDto;
+
+      expect(typeof newBlog.id).toBe('string');
+      expect(new Date(newBlog.createdAt).toString()).not.toBe('Invalid Date');
+      expect(newBlog.name).toBe(blog.name);
+      expect(newBlog.description).toBe(blog.description);
+      expect(newBlog.websiteUrl).toBe(blog.websiteUrl);
+      expect(typeof newBlog.isMembership).toBe('boolean');
+
+      newBlogs.push(newBlog);
+    }
+
+    return newBlogs;
+  }
 
   async getAll(
     query: Partial<GetBlogsQueryParams> = {},
