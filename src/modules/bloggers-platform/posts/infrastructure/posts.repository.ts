b10bbbccb468d433c95ea4f71bdea6ae85860356
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-08-07 23:28:08 +0300

    feat(posts): реализовал методы "getByIdOrNotFoundFail", "updatePost" для PostsRepository

--- src/modules/bloggers-platform/posts/infrastructure/posts.repository.ts
+++ src/modules/bloggers-platform/posts/infrastructure/posts.repository.ts
@@ -3,26 +3,34 @@ import { PG_POOL } from '../../../database/constants/database.constants';
 import { Pool, QueryResult } from 'pg';
 import { PostDbType } from '../types/post-db.type';
 import { CreatePostDto } from '../dto/create-post.dto';
+import { DomainException } from '../../../../core/exceptions/domain-exceptions';
+import { DomainExceptionCode } from '../../../../core/exceptions/domain-exception-codes';
+import { UpdatePostDto } from '../dto/update-post.dto';
 
 @Injectable()
 export class PostsRepository {
   constructor(@Inject(PG_POOL) private readonly pool: Pool) {}
 
-  // async getByIdOrNotFoundFail(id: string): Promise<PostDocument> {
-  //   const post: PostDocument | null = await this.PostModel.findOne({
-  //     _id: id,
-  //     deletedAt: null,
-  //   });
-  //
-  //   if (!post) {
-  //     throw new DomainException({
-  //       code: DomainExceptionCode.NotFound,
-  //       message: `The post with ID (${id}) does not exist`,
-  //     });
-  //   }
-  //
-  //   return post;
-  // }
+  async getByIdOrNotFoundFail(id: number): Promise<PostDbType> {
+    const { rows }: QueryResult<PostDbType> = await this.pool.query(
+      `
+        SELECT *
+        FROM "Posts"
+        WHERE "id" = $1
+          AND "deletedAt" IS NULL
+      `,
+      [id],
+    );
+
+    if (rows.length === 0) {
+      throw new DomainException({
+        code: DomainExceptionCode.NotFound,
+        message: `The post with ID (${id}) does not exist`,
+      });
+    }
+
+    return rows[0];
+  }
 
   async insertPost(dto: CreatePostDto): Promise<number> {
     const { rows }: QueryResult<PostDbType> = await this.pool.query(
@@ -34,5 +42,18 @@ export class PostsRepository {
     );
 
     return rows[0].id;
+  }
+
+  async updatePost(dto: UpdatePostDto) {
+    await this.pool.query(
+      `
+        UPDATE "Posts"
+        SET "title"            = $1,
+            "shortDescription" = $2,
+            "content"          = $3
+        WHERE "id" = $4
+      `,
+      [dto.title, dto.shortDescription, dto.content],
+    );
   }
 }
