c036bad5417a30b036cb5c1ed5c2c330b5e333a4
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-08-07 13:52:57 +0300

    refactor(posts): удалил поле blogId из PostInputDto

--- src/modules/bloggers-platform/posts/api/input-dto/post-input.dto.ts
+++ src/modules/bloggers-platform/posts/api/input-dto/post-input.dto.ts
@@ -1,6 +1,4 @@
 import { IsStringWithTrimDecorator } from '../../../../../core/decorators/validation/is-string-with-trim.decorator';
-import { TrimDecorator } from '../../../../../core/decorators/transform/trim.decorator';
-import { IsString } from 'class-validator';
 
 export class PostInputDto {
   @IsStringWithTrimDecorator(1, 30)
@@ -11,8 +9,4 @@ export class PostInputDto {
 
   @IsStringWithTrimDecorator(1, 1000)
   content: string;
-
-  @IsString()
-  @TrimDecorator()
-  blogId: string;
 }
