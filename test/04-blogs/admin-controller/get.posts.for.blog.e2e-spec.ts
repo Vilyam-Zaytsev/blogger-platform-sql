f729f51545f0f080bb13cd40a082b417f5296b5b
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-08-08 15:44:41 +0300

    test(posts): создал тесты на выборку постов для определенного блога

---
+++ test/04-blogs/admin-controller/get.posts.for.blog.e2e-spec.ts
@@ -0,0 +1,245 @@
+import request, { Response } from 'supertest';
+import { GLOBAL_PREFIX } from '../../../src/setup/global-prefix.setup';
+import { TestLoggers } from '../../helpers/test.loggers';
+import { AppTestManager } from '../../managers/app.test-manager';
+import { AdminCredentials } from '../../types';
+import { Server } from 'http';
+import { BlogViewDto } from 'src/modules/bloggers-platform/blogs/api/view-dto/blog-view.dto';
+import { PaginatedViewDto } from '../../../src/core/dto/paginated.view-dto';
+import { HttpStatus } from '@nestjs/common';
+import { Filter } from '../../helpers/filter';
+import { SortDirection } from '../../../src/core/dto/base.query-params.input-dto';
+import { TestUtils } from '../../helpers/test.utils';
+import { BlogsTestManager } from '../../managers/blogs.test-manager';
+import { PostsTestManager } from '../../managers/posts.test-manager';
+import { PostViewDto } from '../../../src/modules/bloggers-platform/posts/api/view-dto/post-view.dto';
+import {
+  GetPostsQueryParams,
+  PostsSortBy,
+} from '../../../src/modules/bloggers-platform/posts/api/input-dto/get-posts-query-params.input-dto';
+
+describe('BlogsAdminController - getPostsForBlog() (GET: /sa/blogs/{blogId}/posts)', () => {
+  let appTestManager: AppTestManager;
+  let blogsTestManager: BlogsTestManager;
+  let postsTestManager: PostsTestManager;
+  let adminCredentials: AdminCredentials;
+  let adminCredentialsInBase64: string;
+  let testLoggingEnabled: boolean;
+  let server: Server;
+
+  beforeAll(async () => {
+    appTestManager = new AppTestManager();
+    await appTestManager.init();
+
+    adminCredentials = appTestManager.getAdminCredentials();
+    adminCredentialsInBase64 = TestUtils.encodingAdminDataInBase64(
+      adminCredentials.login,
+      adminCredentials.password,
+    );
+    server = appTestManager.getServer();
+    testLoggingEnabled = appTestManager.coreConfig.testLoggingEnabled;
+
+    blogsTestManager = new BlogsTestManager(server, adminCredentialsInBase64);
+    postsTestManager = new PostsTestManager(server, adminCredentialsInBase64);
+  });
+
+  beforeEach(async () => {
+    await appTestManager.cleanupDb(['schema_migrations']);
+  });
+
+  afterAll(async () => {
+    await appTestManager.close();
+  });
+
+  it('should return all posts from a specific blog.', async () => {
+    // 🔻 Создаем один тестовый блог, к которому будут относиться создаваемые посты
+    const [blog]: BlogViewDto[] = await blogsTestManager.createBlog(1);
+
+    // 🔻 Создаем 3 поста, привязанных к вышеуказанному блогу
+    const posts: PostViewDto[] = await postsTestManager.createPost(3, blog.id);
+
+    // 🔻 Выполняем GET-запрос к ручке получения всех постов по конкретному блогу
+    const resGetPosts: Response = await request(server)
+      .get(`/${GLOBAL_PREFIX}/sa/blogs/${blog.id}/posts`)
+      .set('Authorization', adminCredentialsInBase64) // 🔸 Админ авторизован
+      .expect(HttpStatus.OK); // 🔸 Ожидаем успешный ответ 200
+
+    // 🔻 Имитация стандартных query-параметров (по умолчанию):
+    // pageSize = 10, page = 1, sortBy = 'createdAt', sortDirection = 'desc'
+    const query: GetPostsQueryParams = new GetPostsQueryParams();
+
+    // 🔻 Применяем фильтрацию и сортировку к созданным постам так же, как это делает контроллер
+    const filteredCreatedPosts: PostViewDto[] = new Filter<PostViewDto>(posts)
+      .sort({ [query.sortBy]: query.sortDirection }) // 🔸 сортировка по `createdAt` по убыванию
+      .skip(query.calculateSkip()) // 🔸 пропускаем 0 элементов (первая страница)
+      .limit(query.pageSize) // 🔸 берем до 10 элементов (всего 3)
+      .getResult();
+
+    // 🔸 Проверяем, что структура и содержимое ответа совпадает с ожиданиями
+    expect(resGetPosts.body).toEqual({
+      pagesCount: 1,
+      page: 1,
+      pageSize: 10,
+      totalCount: 3,
+      items: filteredCreatedPosts,
+    });
+
+    // 🔸 Убеждаемся, что вернулось ровно 3 поста
+    expect(resGetPosts.body.items).toHaveLength(3);
+
+    if (testLoggingEnabled) {
+      TestLoggers.logE2E(
+        resGetPosts.body,
+        resGetPosts.statusCode,
+        'Test №1: BlogsAdminController - getPostsForBlog() (GET: /sa/blogs/{blogId}/posts)',
+      );
+    }
+  });
+
+  it('should return all entries from a specific blog using the pagination values provided by the client.', async () => {
+    // 🔻 Создаем один тестовый блог
+    const [blog]: BlogViewDto[] = await blogsTestManager.createBlog(1);
+
+    // 🔻 Создаем 12 постов, связанных с этим блогом
+    const posts: PostViewDto[] = await postsTestManager.createPost(12, blog.id);
+
+    // 🔻 Определяем кастомные query-параметры, которые будут переданы клиентом:
+    const query: GetPostsQueryParams = new GetPostsQueryParams();
+    query.sortBy = PostsSortBy.Title; // 🔸 сортировка по заголовку
+    query.sortDirection = SortDirection.Ascending; // 🔸 по возрастанию
+    query.pageNumber = 2; // 🔸 вторая страница
+    query.pageSize = 3; // 🔸 по 3 элемента на страницу
+
+    // 🔻 Отправляем GET-запрос с кастомными параметрами пагинации и сортировки
+    const resGetPosts: Response = await request(server)
+      .get(`/${GLOBAL_PREFIX}/sa/blogs/${blog.id}/posts`)
+      .set('Authorization', adminCredentialsInBase64) // 🔸 админ авторизован
+      .query(query) // 🔸 передаем query-параметры
+      .expect(HttpStatus.OK); // 🔸 ожидаем статус 200 OK
+
+    // 🔻 Применяем к созданным постам ту же сортировку и пагинацию, что и контроллер
+    const filteredCreatedPosts: PostViewDto[] = new Filter<PostViewDto>(posts)
+      .sort({ [query.sortBy]: query.sortDirection }) // 🔸 сортировка по title ASC
+      .skip(query.calculateSkip()) // 🔸 пропускаем (page - 1) * pageSize = 3 элементов
+      .limit(query.pageSize) // 🔸 берем 3 элемента (вторая страница)
+      .getResult();
+
+    // 🔸 Проверяем, что ответ соответствует ожиданиям с учетом переданных query-параметров
+    expect(resGetPosts.body).toEqual({
+      pagesCount: 4, // 🔸 всего 4 страницы (12 / 3)
+      page: 2,
+      pageSize: 3,
+      totalCount: 12,
+      items: filteredCreatedPosts,
+    });
+
+    // 🔸 Убеждаемся, что вернулось ровно 3 поста (вторая страница)
+    expect(resGetPosts.body.items).toHaveLength(3);
+
+    if (testLoggingEnabled) {
+      TestLoggers.logE2E(
+        resGetPosts.body,
+        resGetPosts.statusCode,
+        'Test №2: BlogsAdminController - getPostsForBlog() (GET: /sa/blogs/{blogId}/posts)',
+      );
+    }
+  });
+
+  it('should return a 404 error if the blog does not exist.', async () => {
+    // 🔻 Создаем один тестовый блог
+    const [blog]: BlogViewDto[] = await blogsTestManager.createBlog(1);
+
+    // 🔻 Создаем 12 постов, связанных с этим блогом
+    await postsTestManager.createPost(12, blog.id);
+
+    // 🔻 Указываем несуществующий blogId
+    const incorrectBlogId: string = '1000000';
+
+    // 🔻 Пытаемся получить посты по несуществующему блогу
+    const resGetPosts_1: Response = await request(server)
+      .get(`/${GLOBAL_PREFIX}/sa/blogs/${incorrectBlogId}/posts`)
+      .set('Authorization', adminCredentialsInBase64) // 🔸 админ авторизован
+      .expect(HttpStatus.NOT_FOUND); // 🔸 ожидаем 404 Not Found
+
+    // 🔻 Получаем посты по существующему блогу (контрольный запрос)
+    const resGetPosts_2: Response = await request(server)
+      .get(`/${GLOBAL_PREFIX}/sa/blogs/${blog.id}/posts`)
+      .set('Authorization', adminCredentialsInBase64)
+      .expect(HttpStatus.OK); // 🔸 ожидаем 200 OK
+
+    // 🔸 Убеждаемся, что вернулось 10 постов по умолчанию (pageSize = 10)
+    expect(resGetPosts_2.body.items).toHaveLength(10);
+
+    if (testLoggingEnabled) {
+      TestLoggers.logE2E(
+        resGetPosts_1.body,
+        resGetPosts_1.statusCode,
+        'Test №3: BlogsAdminController - getPostsForBlog() (GET: /sa/blogs/{blogId}/posts)',
+      );
+    }
+  });
+
+  it('should return a 401 error if the admin is not logged in.', async () => {
+    // 🔻 Создаем один тестовый блог
+    const [blog]: BlogViewDto[] = await blogsTestManager.createBlog(1);
+
+    // 🔻 Создаем один пост, привязанный к блогу
+    await postsTestManager.createPost(1, blog.id);
+
+    // 🔻 Выполняем запрос без заголовка авторизации
+    const resGetPosts_1: Response = await request(server)
+      .get(`/${GLOBAL_PREFIX}/sa/blogs/${blog.id}/posts`)
+      .expect(HttpStatus.UNAUTHORIZED); // 🔸 ожидаем 401 Unauthorized
+
+    // 🔻 Повторяем запрос с корректной авторизацией администратора
+    const resGetPosts_2: Response = await request(server)
+      .get(`/${GLOBAL_PREFIX}/sa/blogs/${blog.id}/posts`)
+      .set('Authorization', adminCredentialsInBase64) // 🔸 корректный base64 логин:пароль
+      .expect(HttpStatus.OK); // 🔸 ожидаем 200 OK
+
+    // 🔸 Убеждаемся, что был возвращен ровно 1 пост
+    expect(resGetPosts_2.body.items).toHaveLength(1);
+
+    if (testLoggingEnabled) {
+      TestLoggers.logE2E(
+        resGetPosts_1.body,
+        resGetPosts_1.statusCode,
+        'Test №4: BlogsAdminController - getPostsForBlog() (GET: /sa/blogs/{blogId}/posts)',
+      );
+    }
+  });
+
+  it('should return error 400 if the BlogId is not valid.', async () => {
+    // 🔻 Создаем один тестовый блог
+    const [blog]: BlogViewDto[] = await blogsTestManager.createBlog(1);
+
+    // 🔻 Создаем один пост, привязанный к созданному блогу
+    await postsTestManager.createPost(1, blog.id);
+
+    // 🔸 Определяем некорректный идентификатор блога
+    const invalidBlogId: string = 'a';
+
+    // 🔻 Выполняем запрос с невалидным blogId
+    const resGetPosts_1: Response = await request(server)
+      .get(`/${GLOBAL_PREFIX}/sa/blogs/${invalidBlogId}/posts`)
+      .set('Authorization', adminCredentialsInBase64) // 🔸 Авторизация присутствует
+      .expect(HttpStatus.BAD_REQUEST); // 🔸 Ожидаем 400 Bad Request
+
+    // 🔻 Повторяем запрос с корректным blogId для сравнения
+    const resGetPosts_2: Response = await request(server)
+      .get(`/${GLOBAL_PREFIX}/sa/blogs/${blog.id}/posts`)
+      .set('Authorization', adminCredentialsInBase64)
+      .expect(HttpStatus.OK); // 🔸 Ожидаем 200 OK
+
+    // 🔸 Проверяем, что пост действительно существует (1 шт.)
+    expect(resGetPosts_2.body.items).toHaveLength(1);
+
+    if (testLoggingEnabled) {
+      TestLoggers.logE2E(
+        resGetPosts_1.body,
+        resGetPosts_1.statusCode,
+        'Test №5: BlogsAdminController - getPostsForBlog() (GET: /sa/blogs/{blogId}/posts)',
+      );
+    }
+  });
+});
