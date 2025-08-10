39e957dbab09f2a67484227e36908213aa2b25b5
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-08-10 21:11:05 +0300

    feat(comments): реализовал метод insertComment для CommentsRepository

--- src/modules/bloggers-platform/comments/infrastructure/comments-repository.ts
+++ src/modules/bloggers-platform/comments/infrastructure/comments-repository.ts
@@ -5,10 +5,23 @@ import { Pool, QueryResult } from 'pg';
 import { CommentDbType } from '../types/comment-db.type';
 import { DomainException } from '../../../../core/exceptions/domain-exceptions';
 import { UpdateCommentContentDto } from './dto/update-comment-content.dto';
+import { CreateCommentDomainDto } from '../domain/dto/create-comment.domain-dto';
 
 @Injectable()
 export class CommentsRepository {
   constructor(@Inject(PG_POOL) private readonly pool: Pool) {}
+
+  async insertComment(dto: CreateCommentDomainDto): Promise<number> {
+    const { rows }: QueryResult<{ id: number }> = await this.pool.query(
+      `
+      INSERT INTO "Comments" ("postId", "commentatorId", "content", "commentatorLogin")
+      VALUES ($1, $2, $3, $4) RETURNING "id";
+      `,
+      [dto.postId, dto.commentatorId, dto.content, dto.commentatorLogin],
+    );
+
+    return rows[0].id;
+  }
 
   async getByIdOrNotFoundFail(id: number): Promise<CommentDbType> {
     const { rows }: QueryResult<CommentDbType> = await this.pool.query(
