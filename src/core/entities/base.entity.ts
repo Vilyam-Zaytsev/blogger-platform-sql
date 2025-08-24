4ad523a4390650261bb3abe93f6bb35936de3cf2
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-08-24 18:33:11 +0300

    feat(entities): добавил базовый абстрактный класс для сущностей

---
+++ src/core/entities/base.entity.ts
@@ -0,0 +1,20 @@
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
