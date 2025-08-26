75e3fbcd1ce9481dd2492998690d8cafbe3df398
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-08-26 20:24:14 +0300

    feat(users/auth): реализовал метод create для EmailConfirmationCode

--- src/modules/user-accounts/auth/domain/entities/email-confirmation-code.entity.ts
+++ src/modules/user-accounts/auth/domain/entities/email-confirmation-code.entity.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, JoinColumn, OneToOne, RelationId } from 'typeorm';
 import { BaseEntity } from '../../../../../core/entities/base.entity';
 import { User } from '../../../users/domain/entities/user.entity';
+import { EmailConfirmationCodeCreateDomainDto } from '../dto/email-confirmation-code.create-domain-dto';
 
 export enum ConfirmationStatus {
   Confirmed = 'Confirmed',
@@ -21,7 +22,7 @@ export class EmailConfirmationCode extends BaseEntity {
     type: 'timestamptz',
     nullable: true,
   })
-  public expirationDate: string | null;
+  public expirationDate: Date | null;
 
   @Column({
     type: 'enum',
@@ -29,6 +30,21 @@ export class EmailConfirmationCode extends BaseEntity {
     default: ConfirmationStatus.NotConfirmed,
   })
   public confirmationStatus: ConfirmationStatus;
+
+  //TODO: есть ли необходимость в добавить protected (protected потому что private не получится из за наследования) конструктор для того чтобы закрыть создание экземпляров из вне?
+
+  static create({
+    confirmationCode,
+    expirationDate,
+  }: EmailConfirmationCodeCreateDomainDto): EmailConfirmationCode {
+    const emailConfirmationCode = new this();
+
+    emailConfirmationCode.confirmationCode = confirmationCode;
+    emailConfirmationCode.expirationDate = expirationDate;
+    emailConfirmationCode.confirmationStatus = ConfirmationStatus.NotConfirmed;
+
+    return emailConfirmationCode;
+  }
 
   @OneToOne(() => User, (user) => user.emailConfirmationCode, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'userId' })
