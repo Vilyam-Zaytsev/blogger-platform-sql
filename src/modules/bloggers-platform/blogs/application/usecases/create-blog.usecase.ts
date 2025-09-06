89a6622f265a3b6d539f2d99de14f02fd57132ff
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-09-06 21:02:02 +0300

    refactor(blogs): изменил логику работы CreateBlogUseCase

--- src/modules/bloggers-platform/blogs/application/usecases/create-blog.usecase.ts
+++ src/modules/bloggers-platform/blogs/application/usecases/create-blog.usecase.ts
@@ -1,6 +1,7 @@
 import { BlogsRepository } from '../../infrastructure/blogs.repository';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { BlogInputDto } from '../../api/input-dto/blog-input.dto';
+import { Blog } from '../../domain/entities/blog.entity';
 
 export class CreateBlogCommand {
   constructor(public readonly dto: BlogInputDto) {}
@@ -11,6 +12,8 @@ export class CreateBlogUseCase implements ICommandHandler<CreateBlogCommand> {
   constructor(private readonly blogsRepository: BlogsRepository) {}
 
   async execute({ dto }: CreateBlogCommand): Promise<number> {
-    return await this.blogsRepository.create(dto);
+    const blog: Blog = Blog.create(dto);
+
+    return await this.blogsRepository.save(blog);
   }
 }
