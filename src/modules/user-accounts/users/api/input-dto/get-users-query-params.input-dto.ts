735fdfbc05b58f77a8529d6379d052dbc103a9af
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-09-08 22:59:30 +0300

    refactor(users): изменил правило валидации для полей searchLoginTerm и searchEmailTerm в GetUsersQueryParams

--- src/modules/user-accounts/users/api/input-dto/get-users-query-params.input-dto.ts
+++ src/modules/user-accounts/users/api/input-dto/get-users-query-params.input-dto.ts
@@ -1,5 +1,7 @@
-import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { IsEnum, IsOptional } from 'class-validator';
 import { BaseQueryParams } from '../../../../../core/dto/base.query-params.input-dto';
+import { IsStringWithTrimDecorator } from '../../../../../core/decorators/validation/is-string-with-trim.decorator';
+import { loginConstraints } from '../../domain/entities/user.entity';
 
 export enum UsersSortBy {
   CreatedAt = 'createdAt',
@@ -13,11 +15,11 @@ export class GetUsersQueryParams extends BaseQueryParams<UsersSortBy> {
   @IsEnum(UsersSortBy)
   sortBy: UsersSortBy = UsersSortBy.CreatedAt;
 
-  @IsString()
+  @IsStringWithTrimDecorator(1, loginConstraints.maxLength)
   @IsOptional()
   searchLoginTerm: string | null = null;
 
-  @IsString()
+  @IsStringWithTrimDecorator(1, 255)
   @IsOptional()
   searchEmailTerm: string | null = null;
 }
