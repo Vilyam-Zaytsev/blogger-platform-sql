0bdd05da8623ada9a2e7ff88c85b110dcfa146b4
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-09-08 21:27:33 +0300

    refactor(users): переписан DeleteUserUseCase с ручной проверкой существования пользователя

--- src/modules/user-accounts/users/application/usecases/delete-user.usecase.ts
+++ src/modules/user-accounts/users/application/usecases/delete-user.usecase.ts
@@ -1,5 +1,8 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { UsersRepository } from '../../infrastructure/users.repository';
+import { User } from '../../domain/entities/user.entity';
+import { DomainException } from '../../../../../core/exceptions/domain-exceptions';
+import { DomainExceptionCode } from '../../../../../core/exceptions/domain-exception-codes';
 
 export class DeleteUserCommand {
   constructor(public readonly id: number) {}
@@ -10,7 +13,15 @@ export class DeleteUserUseCase implements ICommandHandler<DeleteUserCommand> {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async execute({ id }: DeleteUserCommand): Promise<void> {
-    await this.usersRepository.getByIdOrNotFoundFail(id);
+    const user: User | null = await this.usersRepository.getById(id);
+
+    if (!user) {
+      throw new DomainException({
+        code: DomainExceptionCode.NotFound,
+        message: `The user with ID (${id}) does not exist`,
+      });
+    }
+
     await this.usersRepository.softDelete(id);
   }
 }
