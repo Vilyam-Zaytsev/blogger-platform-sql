e592505e9d8ec9d34146198958990291504da865
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-09-08 21:29:54 +0300

    refactor(users): переписан метод AuthQueryRepository.me()  с ручной проверкой существования пользователя

--- src/modules/user-accounts/auth/infrastructure/query/auth.query-repository.ts
+++ src/modules/user-accounts/auth/infrastructure/query/auth.query-repository.ts
@@ -2,13 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { UsersRepository } from '../../../users/infrastructure/users.repository';
 import { MeViewDto } from '../../../users/api/view-dto/user.view-dto';
 import { User } from '../../../users/domain/entities/user.entity';
+import { DomainException } from '../../../../../core/exceptions/domain-exceptions';
+import { DomainExceptionCode } from '../../../../../core/exceptions/domain-exception-codes';
 
 @Injectable()
 export class AuthQueryRepository {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async me(id: number): Promise<MeViewDto> {
-    const user: User = await this.usersRepository.getByIdOrNotFoundFail(id);
+    const user: User | null = await this.usersRepository.getById(id);
+
+    if (!user) {
+      throw new DomainException({
+        code: DomainExceptionCode.NotFound,
+        message: `The user with ID (${id}) does not exist`,
+      });
+    }
 
     return MeViewDto.mapToView(user);
   }
