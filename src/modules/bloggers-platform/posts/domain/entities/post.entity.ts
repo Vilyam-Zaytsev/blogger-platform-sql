ac86bad4112d30c931af2d770cc058b07b20c724
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-09-10 16:56:59 +0300

    refactor(posts): вынес ограничения для полей PostEntity в отдельные константы

---
+++ src/modules/bloggers-platform/posts/domain/entities/post.entity.ts
@@ -0,0 +1,14 @@
+export const titleConstraints = {
+  minLength: 1,
+  maxLength: 30,
+};
+
+export const shortDirectionConstraints = {
+  minLength: 1,
+  maxLength: 100,
+};
+
+export const contentConstraints = {
+  minLength: 1,
+  maxLength: 1000,
+};
