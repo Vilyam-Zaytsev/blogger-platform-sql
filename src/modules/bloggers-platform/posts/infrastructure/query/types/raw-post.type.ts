d70f591f746096c2f3ea28ea9abb857865e8fb6e
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-09-27 22:45:14 +0300

    feat(posts): добавлен тип RawPost для типизации сырых данных постов из БД
    - Создан TypeScript тип RawPost с полными данными поста: id, title, shortDescription, content, createdAt
    - Включены поля связанного блога: blogId, blogName
    - Добавлены агрегированные данные по реакциям: likesCount, dislikesCount, myStatus, newestLikes
    - Тип используется для строгой типизации результатов SQL-запросов с join'ами и агрегациями
    - Обеспечивает типобезопасность при работе с данными постов на инфраструктурном уровне и упрощает маппинг в DTO

---
+++ src/modules/bloggers-platform/posts/infrastructure/query/types/raw-post.type.ts
@@ -0,0 +1,16 @@
+import { ReactionStatus } from '../../../../reactions/types/reaction-db.type';
+import { NewestLikes } from '../../../types/newest-likes.type';
+
+export type RawPost = {
+  id: number;
+  title: string;
+  shortDescription: string;
+  content: string;
+  createdAt: Date;
+  blogId: string;
+  blogName: string;
+  likesCount: number;
+  dislikesCount: number;
+  newestLikes: NewestLikes[];
+  myStatus: ReactionStatus;
+};
