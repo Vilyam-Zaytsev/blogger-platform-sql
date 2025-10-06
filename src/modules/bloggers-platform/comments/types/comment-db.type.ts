f0986a28d813c83d1b52defa31f595c284997884
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-10-06 12:47:31 +0300

    refactor(comments): удалил лишние типы

--- src/modules/bloggers-platform/comments/types/comment-db.type.ts
+++
@@ -1,9 +0,0 @@
-export type CommentDb = {
-  id: number;
-  postId: number;
-  commentatorId: number;
-  content: string;
-  createdAt: string;
-  updatedAt: string;
-  deletedAt: string | null;
-};
--- src/modules/bloggers-platform/comments/types/reactions-count.type.ts
+++
@@ -1,4 +0,0 @@
-export type ReactionsCount = {
-  likesCount: number;
-  dislikesCount: number;
-};
