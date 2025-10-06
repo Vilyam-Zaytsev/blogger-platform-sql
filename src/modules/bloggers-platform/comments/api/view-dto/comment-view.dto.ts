57a21f7e6c407b35dc0f8b46afdc82b51dd63c5f
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-10-06 11:32:18 +0300

    refactor(comments-view-dto): выделен тип ReactionsInfo для описания информации о реакциях
    - Тип ReactionsInfo описывает количество лайков, дизлайков и статус текущего пользователя с использованием ReactionStatus
    - Поле likesInfo в CommentViewDto заменено на ReactionsInfo для улучшенной типизации и ясности кода
    - Удалён импорт и использование ReactionsCount, объединена логика в более конкретном и подходящем типе ReactionsInfo
    - Изменение улучшает структуру DTO и делает её более понятной для дальнейшей поддержки

--- src/modules/bloggers-platform/comments/api/view-dto/comment-view.dto.ts
+++ src/modules/bloggers-platform/comments/api/view-dto/comment-view.dto.ts
@@ -1,15 +1,20 @@
 import { ReactionStatus } from '../../../reactions/types/reaction-db.type';
-import { ReactionsCount } from '../../types/reactions-count.type';
 
 export type CommentatorInfo = {
   userId: string;
   userLogin: string;
 };
 
+export type ReactionsInfo = {
+  likesCount: number;
+  dislikesCount: number;
+  myStatus: ReactionStatus;
+};
+
 export class CommentViewDto {
   id: string;
   content: string;
   commentatorInfo: CommentatorInfo;
-  likesInfo: ReactionsCount & { myStatus: ReactionStatus };
+  likesInfo: ReactionsInfo;
   createdAt: string;
 }
