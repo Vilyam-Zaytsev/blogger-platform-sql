4ed006393bc8b0f501bb0cd551dc9d166086952b
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-10-06 10:09:33 +0300

    refactor(comments): миграция CommentsRepository на наследование BaseRepository и подготовка к ORM
    - Переведён CommentsRepository на наследование от BaseRepository с передачей сущности Comment и DataSource из TypeORM
    - Удалены старые SQL-запросы через pg.Pool, методы пока оставлены заглушками для дальнейшей реализации через ORM
    - Сохраны объявления основных методов: update комментария, создание и обновление реакций, получение реакции пользователя по commentId
    - Улучшена архитектура, разделена ответственность, репозиторий приведён к единому паттерну проекта с перспективой полной ORM
    - Это обеспечивает готовую основу для безопасной работы с комментариями и реакциями в дальнейшем с использованием TypeORM

--- src/modules/bloggers-platform/comments/infrastructure/comments-repository.ts
+++ src/modules/bloggers-platform/comments/infrastructure/comments-repository.ts
@@ -1,26 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { UpdateCommentContentDto } from './dto/update-comment-content.dto';
-import { CreateCommentDomainDto } from '../domain/dto/create-comment.domain-dto';
 import { ReactionDb, ReactionStatus } from '../../reactions/types/reaction-db.type';
 import { ReactionCreateDto } from '../../reactions/dto/reaction.create-dto';
-import { CommentDb } from '../types/comment-db.type';
+import { BaseRepository } from '../../../../core/repositories/base.repository';
+import { Comment } from '../domain/entities/comment.entity';
+import { DataSource } from 'typeorm';
 
 @Injectable()
-export class CommentsRepository {
-  constructor() {}
-  async save(): Promise<number> {
-    return 1;
-  }
-
-  async softDelete(id: number): Promise<void> {}
-
-  async getById(id: number): Promise<CommentDb | null> {
-    return id < 2 ? ({} as CommentDb) : null;
-  }
-  // 🔸 Comments:
-
-  async create(dto: CreateCommentDomainDto): Promise<number> {
-    return 1;
+export class CommentsRepository extends BaseRepository<Comment> {
+  constructor(dataSource: DataSource) {
+    super(dataSource, Comment);
   }
 
   async update(dto: UpdateCommentContentDto): Promise<void> {}
@@ -38,147 +27,3 @@ export class CommentsRepository {
     return {} as ReactionDb;
   }
 }
-
-// export class CommentsRepository extends BaseRepository<
-//   CommentDb,
-//   CreateCommentDomainDto,
-//   UpdateCommentContentDto
-// > {
-//   constructor(@Inject(PG_POOL) pool: Pool) {
-//     super(pool, 'Comments');
-//   }
-//
-//   // 🔸 Comments:
-//
-//   async create(types: CreateCommentDomainDto): Promise<number> {
-//     const query = `
-//       INSERT INTO "Comments" ("postId", "commentatorId", "content")
-//       VALUES ($1, $2, $3) RETURNING "id";
-//     `;
-//
-//     const { rows }: QueryResult<{ id: number }> = await this.pool.query(query, [
-//       types.postId,
-//       types.commentatorId,
-//       types.content,
-//     ]);
-//
-//     return rows[0].id;
-//   }
-//// export class CommentsRepository extends BaseRepository<
-//   CommentDb,
-//   CreateCommentDomainDto,
-//   UpdateCommentContentDto
-// > {
-//   constructor(@Inject(PG_POOL) pool: Pool) {
-//     super(pool, 'Comments');
-//   }
-//
-//   // 🔸 Comments:
-//
-//   async create(types: CreateCommentDomainDto): Promise<number> {
-//     const query = `
-//       INSERT INTO "Comments" ("postId", "commentatorId", "content")
-//       VALUES ($1, $2, $3) RETURNING "id";
-//     `;
-//
-//     const { rows }: QueryResult<{ id: number }> = await this.pool.query(query, [
-//       types.postId,
-//       types.commentatorId,
-//       types.content,
-//     ]);
-//
-//     return rows[0].id;
-//   }
-//
-//   async update(types: UpdateCommentContentDto): Promise<void> {
-//     const query = `
-//       UPDATE "Comments"
-//       SET "content" = $1
-//       WHERE "id" = $2
-//     `;
-//
-//     await this.pool.query(query, [types.content, types.commentId]);
-//   }
-//
-//   // 🔸 Reactions:
-//
-//   async createReaction(types: CreateReactionDto): Promise<void> {
-//     const query = `
-//       INSERT INTO "CommentsReactions" ("status", "userId", "commentId")
-//       VALUES ($1, $2, $3)
-//     `;
-//
-//     await this.pool.query(query, [types.status, types.userId, types.parentId]);
-//   }
-//
-//   async updateStatusPostReaction(reactionId: number, newStatus: ReactionStatus): Promise<void> {
-//     const query = `
-//       UPDATE "CommentsReactions"
-//       SET "status" = $1
-//       WHERE "id" = $2
-//     `;
-//
-//     await this.pool.query(query, [newStatus, reactionId]);
-//   }
-//
-//   async getReactionByUserIdAndCommentId(
-//     userId: number,
-//     commentId: number,
-//   ): Promise<ReactionDb | null> {
-//     const query = `
-//       SELECT *
-//       FROM "CommentsReactions"
-//       WHERE "userId" = $1
-//         AND "commentId" = $2
-//     `;
-//     const { rows }: QueryResult<ReactionDb> = await this.pool.query(query, [userId, commentId]);
-//
-//     return rows[0] || null;
-//   }
-
-//   async update(types: UpdateCommentContentDto): Promise<void> {
-//     const query = `
-//       UPDATE "Comments"
-//       SET "content" = $1
-//       WHERE "id" = $2
-//     `;
-//
-//     await this.pool.query(query, [types.content, types.commentId]);
-//   }
-//
-//   // 🔸 Reactions:
-//
-//   async createReaction(types: CreateReactionDto): Promise<void> {
-//     const query = `
-//       INSERT INTO "CommentsReactions" ("status", "userId", "commentId")
-//       VALUES ($1, $2, $3)
-//     `;
-//
-//     await this.pool.query(query, [types.status, types.userId, types.parentId]);
-//   }
-//
-//   async updateStatusPostReaction(reactionId: number, newStatus: ReactionStatus): Promise<void> {
-//     const query = `
-//       UPDATE "CommentsReactions"
-//       SET "status" = $1
-//       WHERE "id" = $2
-//     `;
-//
-//     await this.pool.query(query, [newStatus, reactionId]);
-//   }
-//
-//   async getReactionByUserIdAndCommentId(
-//     userId: number,
-//     commentId: number,
-//   ): Promise<ReactionDb | null> {
-//     const query = `
-//       SELECT *
-//       FROM "CommentsReactions"
-//       WHERE "userId" = $1
-//         AND "commentId" = $2
-//     `;
-//     const { rows }: QueryResult<ReactionDb> = await this.pool.query(query, [userId, commentId]);
-//
-//     return rows[0] || null;
-//   }
-// }
