fb7119bdbeda5dd138ebcbd01b4cf944bc512562
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-10-06 15:02:28 +0300

    refactor(comments-repository): удаление методов реакций из CommentsRepository
    - Удалены методы createReaction, updateStatusPostReaction и getReactionByUserIdAndCommentId из CommentsRepository
    - Репозиторий оставлен чистым, без логики работы с реакциями, для улучшения разделения ответственности
    - Логика реакций перенесена в отдельный ReactionsRepository для работы с сущностью Reaction
    - Упрощение и повышение чистоты архитектуры репозитория комментариев

--- src/modules/bloggers-platform/comments/infrastructure/comments-repository.ts
+++ src/modules/bloggers-platform/comments/infrastructure/comments-repository.ts
@@ -1,29 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { UpdateCommentContentDto } from './dto/update-comment-content.dto';
-import { ReactionCreateDto } from '../../reactions/dto/reaction.create-dto';
 import { BaseRepository } from '../../../../core/repositories/base.repository';
 import { Comment } from '../domain/entities/comment.entity';
 import { DataSource } from 'typeorm';
-import { Reaction, ReactionStatus } from '../../reactions/domain/entities/reaction.entity';
 
 @Injectable()
 export class CommentsRepository extends BaseRepository<Comment> {
   constructor(dataSource: DataSource) {
     super(dataSource, Comment);
-  }
-
-  async update(dto: UpdateCommentContentDto): Promise<void> {}
-
-  // 🔸 Reactions:
-
-  async createReaction(dto: ReactionCreateDto): Promise<void> {}
-
-  async updateStatusPostReaction(reactionId: number, newStatus: ReactionStatus): Promise<void> {}
-
-  async getReactionByUserIdAndCommentId(
-    userId: number,
-    commentId: number,
-  ): Promise<Reaction | null> {
-    return {} as Reaction;
   }
 }
