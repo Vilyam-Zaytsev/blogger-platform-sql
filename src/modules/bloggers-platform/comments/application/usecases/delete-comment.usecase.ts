b8264cb8f64650da5f7f1e9592006d82370a23ca
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-10-06 14:34:46 +0300

    refactor(delete-comment): упростить сигнатуру команды и обработчика
    - Изменён конструктор DeleteCommentCommand: вместо DTO принимает два позиционных аргумента (commentId, userId)
    - Адаптирован метод execute в DeleteCommentUseCase для деструктуризации новых параметров вместо dto
    - Сохранена логика проверки существования комментария и прав пользователя с выбросом DomainException при ошибках
    - Улучшена читаемость и сокращён объём кода за счёт отказа от отдельного DTO-класса

--- src/modules/bloggers-platform/comments/application/usecases/delete-comment.usecase.ts
+++ src/modules/bloggers-platform/comments/application/usecases/delete-comment.usecase.ts
@@ -1,35 +1,37 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { CommentsRepository } from '../../infrastructure/comments-repository';
 import { DomainExceptionCode } from '../../../../../core/exceptions/domain-exception-codes';
-import { DeleteCommentDto } from '../../dto/delete-comment.dto';
 import { DomainException } from '../../../../../core/exceptions/domain-exceptions';
 import { Comment } from '../../domain/entities/comment.entity';
 
 export class DeleteCommentCommand {
-  constructor(public readonly dto: DeleteCommentDto) {}
+  constructor(
+    public readonly commentId: number,
+    public readonly userId: number,
+  ) {}
 }
 
 @CommandHandler(DeleteCommentCommand)
 export class DeleteCommentUseCase implements ICommandHandler<DeleteCommentCommand> {
   constructor(private readonly commentsRepository: CommentsRepository) {}
 
-  async execute({ dto }: DeleteCommentCommand): Promise<void> {
-    const comment: Comment | null = await this.commentsRepository.getById(dto.commentId);
+  async execute({ commentId, userId }: DeleteCommentCommand): Promise<void> {
+    const comment: Comment | null = await this.commentsRepository.getById(commentId);
 
     if (!comment) {
       throw new DomainException({
         code: DomainExceptionCode.NotFound,
-        message: `The comment with ID (${dto.commentId}) does not exist`,
+        message: `The comment with ID (${commentId}) does not exist`,
       });
     }
 
-    if (comment.userId !== dto.userId) {
+    if (comment.userId !== userId) {
       throw new DomainException({
         code: DomainExceptionCode.Forbidden,
-        message: `The user with the ID (${dto.userId}) is not the owner of this comment`,
+        message: `The user with the ID (${userId}) is not the owner of this comment`,
       });
     }
 
-    await this.commentsRepository.softDelete(dto.commentId);
+    await this.commentsRepository.softDelete(commentId);
   }
 }
