6c3986ea93b31b7489ead844474213711d56e5f3
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-10-08 14:23:08 +0300

    chore(database): настройка подключения TypeORM для миграций PostgreSQL
    - Конфигурация соединения включает переменные окружения для хоста, порта, пользователя, пароля и названия базы
    - Указаны папки миграций и сущностей для TypeORM
    - Отключена автоматическая синхронизация схемы, обеспечивая использование миграций для изменений структуры базы данных
    - Включено логирование запросов для отладки миграционных процессов

---
+++ src/migrations-data-source.ts
@@ -0,0 +1,21 @@
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+import { envFilePaths } from './env-file-paths';
+import * as process from 'node:process';
+
+config({
+  path: envFilePaths,
+});
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT ? +process.env.POSTGRES_PORT : 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB_NAME,
+  synchronize: false,
+  logging: true,
+  migrations: [`${__dirname}/modules/database/migrations`],
+  entities: ['src/**/*.entity.ts'],
+});
