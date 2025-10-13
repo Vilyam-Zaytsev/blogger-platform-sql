9a2d1e6fb6742dab40f8ed08a158ef14a3049101
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-10-13 22:46:43 +0300

    feat(quiz): расширен QuizModule интеграцией сущности Question и бизнес-логики
    - Добавлен импорт TypeOrmModule с сущностью Question для работы с БД
    - Подключён UserAccountsModule для связанного функционала пользователей
    - Зарегистрирован контроллер QuestionAdminController для администрирования вопросов
    - Добавлены провайдеры: QuestionRepository, QuestionQueryRepository и CreateQuestionUseCase
    - Обеспечена модульность и масштабируемость квизового функционала в приложении

--- src/modules/quiz/quiz.module.ts
+++ src/modules/quiz/quiz.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Question } from './domain/entities/question.entity';
+import { UserAccountsModule } from '../user-accounts/user-accounts.module';
+import { QuestionAdminController } from './api/question-admin.controller';
+import { QuestionRepository } from './infrastructure/question.repository';
+import { QuestionQueryRepository } from './infrastructure/query/question.query-repository';
+import { CreateQuestionUseCase } from './application/usecases/create-question.usecase';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([Question]), UserAccountsModule],
+  controllers: [QuestionAdminController],
+  providers: [
+    //🔸 Questions:
+    //repo
+    QuestionRepository,
+    QuestionQueryRepository,
+    //use-cases
+    CreateQuestionUseCase,
+  ],
 })
 export class QuizModule {}
