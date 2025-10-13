c43920109b7f4c50a6d0b2834fdea258e4a932a7
Vilyam Zaytsev <z.v.m.365@gmail.com>
2025-10-13 22:33:28 +0300

    feat(quiz): добавлен QuizAdminController с созданием вопросов и защитой BasicAuthGuard
    - Создан контроллер QuizAdminController с маршрутом sa/quiz/questions
    - Метод createQuestion принимает QuestionInputDto, создаёт вопрос через CommandBus и возвращает QuestionViewDto
    - Защита эндпойнта реализована через BasicAuthGuard для ограничения доступа
    - Интегрирован QuestionQueryRepository для получения свежих данных созданного вопроса

---
+++ src/modules/quiz/api/quiz.admin-controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { BasicAuthGuard } from '../../user-accounts/auth/domain/guards/basic/basic-auth.guard';
+import { QuestionInputDto } from './input-dto/question.input-dto';
+import { QuestionViewDto } from './view-dto/question.view-dto';
+import { CommandBus } from '@nestjs/cqrs';
+import { CreateQuestionCommand } from '../application/usecases/create-question.usecase';
+import { QuestionQueryRepository } from '../infrastructure/query/question.query-repository';
+
+@Controller('sa/quiz/questions')
+@UseGuards(BasicAuthGuard)
+export class QuizAdminController {
+  constructor(
+    private readonly commandBus: CommandBus,
+    public readonly questionQueryRepository: QuestionQueryRepository,
+  ) {}
+
+  @Post()
+  async createQuestion(@Body() body: QuestionInputDto): Promise<QuestionViewDto> {
+    const idCreatedQuestion: number = await this.commandBus.execute(
+      new CreateQuestionCommand(body),
+    );
+
+    return this.questionQueryRepository.getByIdOrNotFoundFail(idCreatedQuestion);
+  }
+}
