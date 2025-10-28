52cff61391dc9b4ed25b7176b531143608d9bb71
Vilyam Zautsev <z.v.m.365@gmail.com>
2025-10-28 23:17:18 +0300

    feat(quiz): подключены публичные сущности Game, GameQuestion, Player, Answer в QuizModule

--- src/modules/quiz/quiz.module.ts
+++ src/modules/quiz/quiz.module.ts
@@ -6,9 +6,16 @@ import { QuestionsAdminController } from './admin/api/questions-admin.controller
 import { QuestionsRepository } from './admin/infrastructure/questions-repository';
 import { QuestionsQueryRepository } from './admin/infrastructure/query/questions-query-repository';
 import { CreateQuestionUseCase } from './admin/application/usecases/create-question.usecase';
+import { Game } from './public/domain/entities/game.entity';
+import { GameQuestion } from './public/domain/entities/game-question.entity';
+import { Player } from './public/domain/entities/player.entity';
+import { Answer } from './public/domain/entities/answer.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Question]), UserAccountsModule],
+  imports: [
+    TypeOrmModule.forFeature([Question, Game, GameQuestion, Player, Answer]),
+    UserAccountsModule,
+  ],
   controllers: [QuestionsAdminController],
   providers: [
     //🔸 Questions:
