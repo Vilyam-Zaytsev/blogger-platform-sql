4c248ebbbead3aafe99f6ffec5971a15c68ec991
Vilyam Zautsev <z.v.m.365@gmail.com>
2025-11-02 23:46:41 +0300

    feat(games): добавлены методы для получения игры со статусом Pending и сохранения GameQuestion
    - В GamesRepository реализован метод getGameInPending для поиска игры с статусом Pending
    - Добавлен метод saveGameQuestion для сохранения сущности GameQuestion через dataSource репозиторий
    - Возвращает id сохраненного GameQuestion для дальнейшего использования
    - Эти методы упрощают бизнес-логику работы с играми и вопросами в игровом процессе

--- src/modules/quiz/public/infrastructure/games.repository.ts
+++ src/modules/quiz/public/infrastructure/games.repository.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { BaseRepository } from '../../../../core/repositories/base.repository';
 import { DataSource } from 'typeorm';
-import { Game } from '../domain/entities/game.entity';
+import { Game, GameStatus } from '../domain/entities/game.entity';
+import { GameQuestion } from '../domain/entities/game-question.entity';
 
 @Injectable()
 export class GamesRepository extends BaseRepository<Game> {
   constructor(dataSource: DataSource) {
     super(dataSource, Game);
+  }
+
+  async getGameInPending(): Promise<Game | null> {
+    return await this.repository.findOne({ where: { status: GameStatus.Pending } });
+  }
+
+  async saveGameQuestion(gameQuestion: GameQuestion): Promise<number> {
+    const { id }: GameQuestion = await this.dataSource
+      .getRepository<GameQuestion>(GameQuestion)
+      .save(gameQuestion);
+
+    return id;
   }
 }
