fd9915e71e7ea3587563a195b9c0db6a82ec3473
Vilyam Zautsev <z.v.m.365@gmail.com>
2025-11-02 23:44:59 +0300

    feat(players): добавлен PlayersRepository с методом поиска игрока по userId и статусу игры
    - Создан новый репозиторий PlayersRepository, наследующий BaseRepository<Player>
    - Добавлен метод getPlayerByUserIdInPendingOrActiveGame для получения игрока с указанным userId,
      участвующего в игре со статусом Pending или Active
    - Метод осуществляет поиск с загрузкой связанных сущностей user и game
    - Обеспечивает удобный инструмент для бизнес-логики, связанной с динамикой состояния игрока в игре

---
+++ src/modules/quiz/public/infrastructure/players.repository.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { BaseRepository } from '../../../../core/repositories/base.repository';
+import { Player } from '../domain/entities/player.entity';
+import { DataSource, In } from 'typeorm';
+import { GameStatus } from '../domain/entities/game.entity';
+
+@Injectable()
+export class PlayersRepository extends BaseRepository<Player> {
+  constructor(dataSource: DataSource) {
+    super(dataSource, Player);
+  }
+
+  async getPlayerByUserIdInPendingOrActiveGame(userId: number): Promise<Player | null> {
+    return await this.repository.findOne({
+      where: {
+        userId,
+        game: {
+          status: In([GameStatus.Pending, GameStatus.Active]),
+        },
+      },
+      relations: {
+        user: true,
+        game: true,
+      },
+    });
+  }
+}
