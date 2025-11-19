694e486dd08bc755fff8ef40d753dece9752a33b
Vilyam Zautsev <z.v.m.365@gmail.com>
2025-11-19 13:32:42 +0300

    feat(quiz): добавить GetCurrentGameQueryHandler с валидацией участия
    Новый Query для получения текущей Pending/Active игры пользователя
    с проверкой участия через PlayerValidationService.

---
+++ src/modules/quiz/public/application/queries/get-current-game.query-handler.ts
@@ -0,0 +1,24 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { GameViewDto } from '../../api/view-dto/game.view-dto';
+import { PlayerValidationService } from '../../domain/services/player-validation.service';
+import { GamesQueryRepository } from '../../infrastructure/query/games.query-repository';
+import { TypeId } from '../types/type-id.type';
+
+export class GetCurrentGameQuery {
+  constructor(public readonly userId: number) {}
+}
+
+@QueryHandler(GetCurrentGameQuery)
+export class GetCurrentGameQueryHandler implements IQueryHandler<GetCurrentGameQuery, GameViewDto> {
+  constructor(
+    private readonly gamesQueryRepository: GamesQueryRepository,
+    private readonly playersValidationService: PlayerValidationService,
+  ) {}
+
+  async execute({ userId }: GetCurrentGameQuery): Promise<GameViewDto> {
+    const gameId: number =
+      await this.playersValidationService.ensureUserInPendingOrActiveGame(userId);
+
+    return this.gamesQueryRepository.getByIdOrNotFoundFail(gameId, TypeId.id);
+  }
+}
