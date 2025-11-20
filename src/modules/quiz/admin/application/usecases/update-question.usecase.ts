384d734e9929c421d89e35a6e2f472562254186f
Vilyam Zautsev <z.v.m.365@gmail.com>
2025-11-20 13:59:00 +0300

    refactor(admin): обновить UpdateQuestionUseCase на publicId с валидатором
    - Использовать getByPublicId вместо getById для поиска по publicId
    - Добавить QuestionValidatorService для централизованной валидации
    - Заменить встроенную валидацию на validateQuestionExists и validateQuestionBeforeUpdate
    - Удалить дублирующиеся импорты QuestionStatus и DomainException/ValidationException
    - Использовать возвращаемое значение existingQuestion из валидатора

--- src/modules/quiz/admin/application/usecases/update-question.usecase.ts
+++ src/modules/quiz/admin/application/usecases/update-question.usecase.ts
@@ -1,10 +1,8 @@
 import { QuestionUpdateDto } from '../dto/question.update-dto';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { QuestionsRepository } from '../../infrastructure/questions-repository';
-import { Question, QuestionStatus } from '../../domain/entities/question.entity';
-import { DomainException } from '../../../../../core/exceptions/domain-exceptions';
-import { DomainExceptionCode } from '../../../../../core/exceptions/domain-exception-codes';
-import { ValidationException } from '../../../../../core/exceptions/validation-exception';
+import { Question } from '../../domain/entities/question.entity';
+import { QuestionValidatorService } from '../../domain/services/question-validator.service';
 
 export class UpdateQuestionCommand {
   constructor(public readonly dto: QuestionUpdateDto) {}
@@ -12,28 +10,24 @@ export class UpdateQuestionCommand {
 
 @CommandHandler(UpdateQuestionCommand)
 export class UpdateQuestionUseCase implements ICommandHandler<UpdateQuestionCommand> {
-  constructor(private readonly questionsRepository: QuestionsRepository) {}
+  constructor(
+    private readonly questionsRepository: QuestionsRepository,
+    private readonly questionValidatorService: QuestionValidatorService,
+  ) {}
 
   async execute({ dto }: UpdateQuestionCommand): Promise<void> {
-    const question: Question | null = await this.questionsRepository.getById(dto.id);
+    const question: Question | null = await this.questionsRepository.getByPublicId(dto.id);
+    const existingQuestion: Question = this.questionValidatorService.validateQuestionExists(
+      question,
+      dto.id,
+    );
 
-    if (!question) {
-      throw new DomainException({
-        code: DomainExceptionCode.NotFound,
-        message: `The question with ID (${dto.id}) does not exist`,
-      });
-    }
+    this.questionValidatorService.validateQuestionBeforeUpdate(
+      existingQuestion,
+      dto.correctAnswers,
+    );
 
-    if (question.status === QuestionStatus.Published && dto.correctAnswers.length < 1) {
-      throw new ValidationException([
-        {
-          message: `Cannot publish question without correct answers`,
-          field: 'correctAnswers',
-        },
-      ]);
-    }
-
-    question.update(dto);
-    await this.questionsRepository.save(question);
+    existingQuestion.update(dto);
+    await this.questionsRepository.save(existingQuestion);
   }
 }
