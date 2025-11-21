3950a3a07e3e81d447f89d6c29963f748dacfa80
Vilyam Zautsev <z.v.m.365@gmail.com>
2025-11-21 22:49:00 +0300

    refactor(public): delete dto

--- src/modules/quiz/public/domain/dto/award-points.dto.ts
+++
@@ -1,8 +0,0 @@
-import { AnswerStatus } from '../entities/answer.entity';
-
-export class AwardPointsDto {
-  playerId: number;
-  answerStatus: AnswerStatus;
-  questionOrder: number;
-  opponentAnswersCount: number;
-}
