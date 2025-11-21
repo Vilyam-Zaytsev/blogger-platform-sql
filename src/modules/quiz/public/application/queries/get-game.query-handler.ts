18faaa6ff4d4a3acae547295824ca7ae73de342f
Vilyam Zautsev <z.v.m.365@gmail.com>
2025-11-21 22:52:37 +0300

    refactor(public): обновить GetGameQueryHandler на числовой ID и переместить валидацию
    - Изменить тип gameId в команде с string на number
    - Удалить инъекцию PlayerValidationService
    - Удалить импорты TypeId и PlayerValidationService
    - Добавить импорты DomainException и DomainExceptionCode
    - Переместить валидацию ensureUserParticipatesInCurrentGame в private метод handler'а
    - Обновить вызов getByIdOrNotFoundFail: удалить параметр TypeId
    - Явно конвертировать player.id в число через Number() для сравнения с userId

--- src/modules/quiz/public/application/queries/get-game.query-handler.ts
+++ src/modules/quiz/public/application/queries/get-game.query-handler.ts
@@ -1,31 +1,37 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { GameViewDto } from '../../api/view-dto/game.view-dto';
 import { GamesQueryRepository } from '../../infrastructure/query/games.query-repository';
-import { PlayerValidationService } from '../../domain/services/player-validation.service';
-import { TypeId } from '../../../types/type-id.type';
+import { DomainException } from '../../../../../core/exceptions/domain-exceptions';
+import { DomainExceptionCode } from '../../../../../core/exceptions/domain-exception-codes';
 
 export class GetGameQuery {
   constructor(
     public readonly userId: number,
-    public readonly gameId: string,
+    public readonly gameId: number,
   ) {}
 }
 
 @QueryHandler(GetGameQuery)
 export class GetGameQueryHandler implements IQueryHandler<GetGameQuery, GameViewDto> {
-  constructor(
-    private readonly gameQueryRepository: GamesQueryRepository,
-    private readonly playerValidationService: PlayerValidationService,
-  ) {}
+  constructor(private readonly gameQueryRepository: GamesQueryRepository) {}
 
   async execute({ gameId, userId }: GetGameQuery): Promise<GameViewDto> {
-    const game: GameViewDto = await this.gameQueryRepository.getByIdOrNotFoundFail(
-      gameId,
-      TypeId.publicId,
-    );
+    const game: GameViewDto = await this.gameQueryRepository.getByIdOrNotFoundFail(gameId);
 
-    this.playerValidationService.ensureUserParticipatesInCurrentGame(userId, game);
+    this.ensureUserParticipatesInCurrentGame(userId, game);
 
     return game;
+  }
+
+  private ensureUserParticipatesInCurrentGame(userId: number, game: GameViewDto): void {
+    if (
+      Number(game.firstPlayerProgress.player.id) !== userId &&
+      Number(game.secondPlayerProgress?.player.id) !== userId
+    ) {
+      throw new DomainException({
+        code: DomainExceptionCode.Forbidden,
+        message: `User with id ${userId} is not a participant of game with id ${game.id}`,
+      });
+    }
   }
 }
