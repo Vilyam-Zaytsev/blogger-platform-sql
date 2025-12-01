8aa89afcb4c84faf460e2145f57a703ff3048fee
Vilyam Zautsev <z.v.m.365@gmail.com>
2025-12-01 12:15:16 +0300

    feat(public): добавить RawTopPlayer тип для SQL результата рейтинга

---
+++ src/modules/quiz/public/infrastructure/query/types/raw-top-players.type.ts
@@ -0,0 +1,10 @@
+export type RawTopPlayer = {
+  userId: number;
+  userLogin: string;
+  sumScore: string;
+  avgScores: string;
+  gamesCount: string;
+  winsCount: string;
+  lossesCount: string;
+  drawsCount: string;
+};
