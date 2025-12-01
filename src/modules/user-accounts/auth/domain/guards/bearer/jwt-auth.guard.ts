23992355a6a15e44d425bc2ebc15ddfe31584cbd
Vilyam Zautsev <z.v.m.365@gmail.com>
2025-12-01 12:13:00 +0300

    refactor(auth): добавить поддержку @Public() декоратора в JwtAuthGuard
    - Внедрить Reflector для проверки метаданных эндпоинта
    - Добавить метод canActivate() для перехвата запроса до валидации JWT
    - Проверить наличие IS_PUBLIC_KEY метаданных через getAllAndOverride()
    - Пропустить валидацию JWT для публичных эндпоинтов (return true)
    - Вызвать super.canActivate() для стандартной JWT валидации на остальных эндпоинтах
    - Сохранить существующий handleRequest() для обработки ошибок аутентификации
    - Позволяет помечать определенные маршруты как @Public() без необходимости JWT токена
    - Использует @UseGuards(JwtAuthGuard) на контроллере + @Public() на методе для переопределения

--- src/modules/user-accounts/auth/domain/guards/bearer/jwt-auth.guard.ts
+++ src/modules/user-accounts/auth/domain/guards/bearer/jwt-auth.guard.ts
@@ -1,11 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UserContextDto } from '../dto/user-context.dto';
 import { DomainException } from '../../../../../../core/exceptions/domain-exceptions';
 import { DomainExceptionCode } from '../../../../../../core/exceptions/domain-exception-codes';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../../../../decorators/public.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic: boolean = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
+
   handleRequest<TUser = UserContextDto>(err: any, user: any): TUser {
     if (err || !user) {
       throw new DomainException({
